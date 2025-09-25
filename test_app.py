29717fa1a9d70fa87bb139e0ed64d471ec22245a
Strivion <126996579+Strivion@users.noreply.github.com>
2025-09-25 16:02:24 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,9 +1,11 @@
 # test_app.py
-from app import app  # rename 'app' to your module name if needed
+from app import app  # calls the app in question for testing
 
 def test_about_returns_200_and_json():
+    # tests the first endpoint of my API
     client = app.test_client()
     resp = client.get("/about")
     assert resp.status_code == 200
     data = resp.get_json()
     assert "name" in data
+
